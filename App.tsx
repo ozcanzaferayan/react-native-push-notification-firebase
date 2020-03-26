a1617abbfb7a68108a0c66b7a3c88e5ec27322d0
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-03-26 23:54:52 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable prettier/prettier */
+
+import React, {useEffect} from 'react';
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
+import {Platform, PushNotificationIOS} from 'react-native';
+import {FirebaseMessagingTypes} from '@react-native-firebase/messaging';
+
+const App = () => {
+  const getToken = () => {
+    firebase
+      .messaging()
+      .getToken(firebase.app().options.messagingSenderId)
+      .then(x => console.log(x))
+      .catch(e => console.log(e));
+  };
+
+  const registerForRemoteMessages = () => {
+    firebase
+      .messaging()
+      .registerDeviceForRemoteMessages()
+      .then(() => {
+        console.log('Registered');
+        requestPermissions();
+      })
+      .catch(e => console.log(e));
+  };
+
+  const requestPermissions = () => {
+    firebase
+      .messaging()
+      .requestPermission()
+      .then((status: FirebaseMessagingTypes.IOSAuthorizationStatus) => {
+        if (
+          status === FirebaseMessagingTypes.IOSAuthorizationStatus.AUTHORIZED
+        ) {
+          console.log('Authorized');
+          onMessage();
+        } else {
+          console.log('Not authorized');
+        }
+      })
+      .catch(e => console.log(e));
+  };
+
+  const onMessage = () => {
+    firebase.messaging().onMessage(response => {
+      showNotification(response.notification!);
+    });
+  };
+
+  const showNotification = (
+    notification: FirebaseMessagingTypes.Notification,
+  ) => {
+    console.log('Showing notification');
+    console.log(JSON.stringify(PushNotification));
+    PushNotification.localNotification({
+      title: notification.title,
+      message: notification.body!,
+    });
+  };
+
+  getToken();
+  if (Platform.OS === 'ios') {
+    registerForRemoteMessages();
+  } else {
+    onMessage();
+  }
+  return <></>;
+};
+
+export default App;
---
+++ ios/SampleRNPushNotifications/AppDelegate.m
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "AppDelegate.h"
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+
+#import <RNCPushNotificationIOS.h>
+#import <UserNotifications/UserNotifications.h>
+@import Firebase;
+
+@implementation AppDelegate
+
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+ [RNCPushNotificationIOS didRegisterUserNotificationSettings:notificationSettings];
+}
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+ [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+ [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
+}
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+ [RNCPushNotificationIOS didReceiveLocalNotification:notification];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"SampleRNPushNotifications"
+                                            initialProperties:nil];
+
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
+  [FIRApp configure];
+  return YES;
+}
+
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+  completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+@end
